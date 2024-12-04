534ebdd2731f26643237efd7a5d9306456c0b333
Stephen Haney <stephen.haney@gmail.com>
2024-12-04 22:13:19 +0000

    Improving performance by not causing a layout

--- request-before-paint.ts
+++ request-before-paint.ts
@@ -9,61 +9,57 @@
  * If you update an observed property it may trigger React re-renders
  * which will cause layout recalcs - useDeferredValue in React is one way to avoid this
  *
- * This class adds a div to the DOM and resizes it in every RAF so that we will always get
- * a ResizeObserver callback - which happens after layout is calculated but before paint
+ * This class triggers a ResizeObserver callback every frame - which happens after layout is calculated but before paint
  * This is a great time to measure DOM changes and draw to canvas/WebGL to keep them in sync
  */
-class RequestBeforePaint {
-    #targetElem: HTMLDivElement | null = null;
-    #observer: ResizeObserver | null = null;
-  
-    constructor(callback: () => void) {
-      // Create minimal target element
-      this.#targetElem = document.createElement('div');
-      this.#targetElem.style.cssText = [
-        'position:fixed',
-        'width:1px',
-        'height:1px',
-        'right:0',
-        'bottom:0',
-        'pointer-events:none',
-        'contain:strict',
-        'z-index:-999',
-        'visibility:hidden',
-      ].join(';');
-      document.body.appendChild(this.#targetElem);
-  
-      // Start our loop that will resize the div in every frame
-      requestAnimationFrame(this.update);
-  
-      // Wait to create the RO until after syncronously-created-ROs from app code have run (hopefully placing these callbacks last)
-      Promise.resolve().then(() => {
-        this.#observer = new ResizeObserver(callback);
-  
-        this.#observer.observe(this.#targetElem!);
-      });
-    }
-  
-    lastIndex = 0;
-    values = ['1px', '2px'];
-    update = () => {
-      if (!this.#targetElem) return;
-  
-      this.#targetElem.style.width = this.values[this.lastIndex]!;
-      this.lastIndex = this.lastIndex === 1 ? 0 : 1;
-  
-      requestAnimationFrame(this.update);
-    };
-  
-    dispose = () => {
-      if (this.#observer) {
-        this.#observer.disconnect();
-        this.#observer = null;
-      }
-      if (this.#targetElem && this.#targetElem.parentNode) {
-        this.#targetElem.remove();
-      }
-      this.#targetElem = null;
-    };
+class LoopBeforePaint {
+  #dummyElem: HTMLDivElement | null = null;
+  #observer: ResizeObserver | null = null;
+
+  constructor(callback: () => void) {
+    // Create dummy element to observe that will never resize other than when it is added or removed to the DOM
+    this.#dummyElem = document.createElement('div');
+    this.#dummyElem.style.cssText = [
+      'position:fixed',
+      'width:1px',
+      'height:1px',
+      'right:-1px',
+      'bottom:-1px',
+      'pointer-events:none',
+      'contain:strict',
+      'display:none',
+    ].join(';');
+    document.body.appendChild(this.#dummyElem);
+
+    // Start our loop that will trigger a RO callback in every frame
+    requestAnimationFrame(this.update);
+
+    // Wait to create the RO until after syncronously-created-ROs from app code have run (hopefully placing these callbacks last)
+    Promise.resolve().then(() => {
+      this.#observer = new ResizeObserver(callback);
+
+      this.#observer.observe(this.#dummyElem!);
+    });
   }
-  
+
+  update = () => {
+    if (!this.#dummyElem || !this.#observer) return;
+    // cycling disconnect and observe is the fastest way to trigger a resize observer callback
+    // much faster than resizing a div, which triggers browser layout calcs
+    this.#observer.disconnect();
+    this.#observer.observe(this.#dummyElem);
+
+    requestAnimationFrame(this.update);
+  };
+
+  dispose = () => {
+    if (this.#observer) {
+      this.#observer.disconnect();
+      this.#observer = null;
+    }
+    if (this.#dummyElem && this.#dummyElem.parentNode) {
+      this.#dummyElem.remove();
+    }
+    this.#dummyElem = null;
+  };
+}
